ac6ffe85d60d92c45ea03767a0d5e3f10c37d2de
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-11-30 23:37:19 +0530

    Fixing deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,22 +13,22 @@ export default  function Home() {
   );
 }
 
-async function CrudShowcase() {
-  // const session = await getServerAuthSession();
-  // if (!session?.user) return null;
+// async function CrudShowcase() {
+//   // const session = await getServerAuthSession();
+//   // if (!session?.user) return null;
 
-  const latestPost = await api.post.getLatest.query();
+//   const latestPost = await api.post.getLatest.query();
 
-  return (
-    <div className="w-full max-w-xs">
-      {latestPost ? (
-        <p className="truncate">Your most recent post: {latestPost.name}</p>
-      ) : (
-        <p>You have no posts yet.</p>
-      )}
+//   return (
+//     <div className="w-full max-w-xs">
+//       {latestPost ? (
+//         <p className="truncate">Your most recent post: {latestPost.name}</p>
+//       ) : (
+//         <p>You have no posts yet.</p>
+//       )}
 
-      <CreatePost />
-    </div>
-  );
-}
-CrudShowcase()
+//       <CreatePost />
+//     </div>
+//   );
+// }
+// CrudShowcase()
